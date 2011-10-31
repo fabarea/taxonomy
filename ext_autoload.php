1da0543941fcda12f40a373dbecfad82af765120
Steffen Ritter <info@rs-websystems.de>
2011-10-31 10:39:48 +0100

    [TASK] deleted stuff which on first sight is not needed for an BLE basic release

--- ext_autoload.php
+++ ext_autoload.php
@@ -5,12 +5,5 @@
  * $Id: $
  */
 return array(
-	'tx_taxonomy_service_extdirect_controller_abstracthandler' => t3lib_extMgm::extPath('taxonomy', 'Classes/Service/ExtDirect/Controller/AbstractHandler.php'),
-	'tx_taxonomy_domain_repository_conceptrepository' => t3lib_extMgm::extPath('taxonomy', 'Classes/Domain/Repository/ConceptRepository.php'),
-	'tx_taxonomy_domain_model_concept' => t3lib_extMgm::extPath('taxonomy', 'Classes/Domain/Model/Concept.php'),
-	'tx_taxonomy_service_extdirect_controller_abstractdataprovider' => t3lib_extMgm::extPath('taxonomy', 'Classes/Service/ExtDirect/Controller/AbstractDataProvider.php'),
-	'tx_taxonomy_service_extdirect_controller_dataprovider' => t3lib_extMgm::extPath('taxonomy', 'Classes/Service/ExtDirect/Controller/DataProvider.php'),
-	'tx_taxonomy_service_extdirect_controller_abstracttree' => t3lib_extMgm::extPath('taxonomy', 'Classes/Service/ExtDirect/Controller/AbstractTree.php'),
-	'tx_taxonomy_service_extdirect_controller_tree' => t3lib_extMgm::extPath('taxonomy', 'Classes/Service/ExtDirect/Controller/Tree.php'),
 );
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -3,37 +3,6 @@ if (!defined ('TYPO3_MODE')){
 	die ('Access denied.');
 }
 
-// Override ExtJS Theme for ExtJS 4 compatibility
-// @todo: remove this hook when TYPO3 v4 will be compatible with ExtJS 4
-if ($GLOBALS['_GET']['M'] == 'user_TaxonomyTxTaxonomyM1') {
-	$GLOBALS['TBE_STYLES']['extJS']['theme'] = t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Libraries/ExtJS/resources/css/ext-all-gray.css';
-}
-
-if (TYPO3_MODE === 'BE') {
-
-	/**
-	 * Registers a Backend Module
-	 */
-	Tx_Extbase_Utility_Extension::registerModule(
-		$_EXTKEY,
-		'user',	 // Make module a submodule of 'web'
-		'tx_taxonomy_m1',	// Submodule key
-		'',						// Position
-		array(
-			# list, show, new, create, edit, update, delete
-			'Concept' => 'list', 
-		),
-		array(
-			'access' => 'user,group',
-			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
-			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_admin.xml',
-		)
-	);
-}
-
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Taxonomy');
-
-
 t3lib_extMgm::addLLrefForTCAdescr('tx_taxonomy_domain_model_conceptscheme', 'EXT:taxonomy/Resources/Private/Language/locallang_csh_tx_taxonomy_domain_model_conceptscheme.xml');
 t3lib_extMgm::allowTableOnStandardPages('tx_taxonomy_domain_model_conceptscheme');
 $TCA['tx_taxonomy_domain_model_conceptscheme'] = array(
@@ -110,32 +79,6 @@ $TCA['tx_taxonomy_domain_model_lexicallabel'] = array(
 			),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/LexicalLabel.php',
 		'iconfile' 			=> t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_taxonomy_domain_model_lexicallabel.gif'
-	),
-);
-
-t3lib_extMgm::addLLrefForTCAdescr('tx_taxonomy_domain_model_collection', 'EXT:taxonomy/Resources/Private/Language/locallang_csh_tx_taxonomy_domain_model_collection.xml');
-t3lib_extMgm::allowTableOnStandardPages('tx_taxonomy_domain_model_collection');
-$TCA['tx_taxonomy_domain_model_collection'] = array(
-	'ctrl' => array(
-		'title'				=> 'LLL:EXT:taxonomy/Resources/Private/Language/locallang_db.xml:tx_taxonomy_domain_model_collection',
-		'label' 			=> 'ordered',
-		'tstamp' 			=> 'tstamp',
-		'crdate' 			=> 'crdate',
-		'dividers2tabs' => true,
-		'versioningWS' 		=> 2,
-		'versioning_followPages'	=> TRUE,
-		'origUid' 			=> 't3_origuid',
-		'languageField' 	=> 'sys_language_uid',
-		'transOrigPointerField' 	=> 'l10n_parent',
-		'transOrigDiffSourceField' 	=> 'l10n_diffsource',
-		'delete' 			=> 'deleted',
-		'enablecolumns' 	=> array(
-			'disabled' => 'hidden',
-			'starttime' => 'starttime',
-			'endtime' => 'endtime',
-			),
-		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Collection.php',
-		'iconfile' 			=> t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_taxonomy_domain_model_collection.gif'
 	),
 );
 
